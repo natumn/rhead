eafe095a73a4bbfb1680c618e63a0deb792ddc77
natumn <natsume.pcuse75@gmail.com>
2017-12-19 17:29:42 +0900

    [Add] head fn（途中）

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ extern crate getopts;
 
 use std::{env, process};
 use getopts::Options;
-// use std::fs::File;
-// use std::path::Path;
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead, BufReader};
+use std::error::Error;
 
 fn print_usage(program: &str, opts: &Options) {
     let brief = format!("Usage: {} FILE [options]", program);
@@ -11,25 +13,57 @@ fn print_usage(program: &str, opts: &Options) {
     process::exit(0);
 }
 
-fn do_work(inp: &str, out: Option<String>) {
+/*
+fn do_work(inp: &str, lines: Option<String>, filename: Option<String>) {
     println!("{}", inp);
-    match out {
+    match lines {
         Some(x) => println!("{}", x),
-        None => println!("No Output"),
+        None => println!("No lines"),
+    }
+    match filename {
+        Some(x) => println!("{}", x),
+        None => println!("No File"),
     }
 }
-/*
-fn do_head(file: File, lines: i32) {
-    
-}
 */
+
+fn head(lines: i32, filename: String) -> i32 {
+    // ファイル読み込み
+    let string = String::from(filename);
+    let path = Path::new(&string);
+    let display = path.display();
+    let mut file = BufReader::new(File::open(&path).unwrap());
+
+    if lines <= 0 {
+        return 0;
+    } else {
+        let mut buf = String::new();
+        let mut count = 0;
+
+        let mut done = false;
+        while !done {
+            file.read_line(&mut buf).expect(
+                "reading from cursor won't fail",
+            );
+            println!("{:?}", buf);
+            count += 1;
+            if lines == count {
+                done = true;
+            }
+            buf.clear();
+        }
+        return 0;
+    }
+}
+
 fn main() {
     //argsを取得
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
+    let filename = args[4].clone();
 
-    let mut opts = Options::new();
     // optionの設定
+    let mut opts = Options::new();
     opts.optopt("n", "", "set number of lines", "LINES");
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(&args[1..]) {
@@ -39,12 +73,18 @@ fn main() {
     if matches.opt_present("h") {
         print_usage(&program, &opts);
     }
-    let output = matches.opt_str("n");
+    // let lines = matches.opt_str("n");
+    let lines_string = match matches.opt_str("n") {
+        Some(x) => x,
+        None => process::exit(0),
+    };
+    let lines_i32 = lines_string.parse::<i32>().unwrap();
     let input = if !matches.free.is_empty() {
         matches.free[0].clone()
     } else {
         print_usage(&program, &opts);
         return;
     };
-    do_work(&input, output);
+
+    process::exit(head(lines_i32, filename));
 }
